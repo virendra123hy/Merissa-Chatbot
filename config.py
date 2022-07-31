3ee8567aaaf339f401a18713ced2e5ca3b5c643d
virendra123hy <74032762+virendra123hy@users.noreply.github.com>
2022-07-31 20:57:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       MERISSA_TOKEN = os.environ.get("MERISSA_TOKEN", "")
-      OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "NoobxCoder")
+      OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
       BOT_NAME = os.environ.get("BOT_NAME", "")
       OWNER_NAME = os.environ.get("OWNER_NAME", "")
       LANGUAGE_CODE = os.environ.get("LANGUAGE", "lang")
